bf0d53e7e6b6f39933dd3c095db980c952ea4fae
Michelle Pandojo Lukman <michellepandojo@gmail.com>
2025-03-26 12:55:01 +0700

    tambah accent colour, dan component collection card

---
+++ goppo/Views/Helper/CollectionCard.swift
@@ -0,0 +1,51 @@
+///
+//  Collection Card.swift
+//  GOPPO Practice
+//
+//  Created by Michelle PL on 25/03/25.
+//
+
+import SwiftUI
+
+struct Collection_Card: View {
+    var body: some View {
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white) // Bisa diganti dengan .ultraThinMaterial untuk efek blur
+            
+            VStack(alignment: .leading, spacing: 10.0){
+                Image("b_bakso_besar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 124, height: 124)
+                    .cornerRadius(10)
+                
+                VStack(alignment: .leading, spacing: 4.0){
+                    Text("Koleksi 1")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                    Text("Rp 15.000")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                    }
+                
+                Button(action: {} ){
+                    Text("Pesan")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 124, height: 32)
+                        .background(Color.accent)
+                        .cornerRadius(12)
+                }
+                }
+            }
+        .frame(width: 140, height: 232)
+        
+    }
+}
+
+#Preview {
+    Collection_Card()
+}
