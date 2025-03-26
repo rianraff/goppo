725de174adb595aca7f014db68615a056d79ce5b
rianraff <aldrian.raffi@ui.ac.id>
2025-03-26 12:10:48 +0700

    Cleaning

---
+++ goppo/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  goppo
+//
+//  Created by Aldrian Raffi Wicaksono on 26/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
