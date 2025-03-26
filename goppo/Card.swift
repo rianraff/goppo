bc4ceb2e35c5ca229cd0401dd41a478e24092b69
Michelle Pandojo Lukman <michellepandojo@gmail.com>
2025-03-26 11:52:41 +0700

    ini buat card

---
+++ goppo/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  goppo
+//
+//  Created by Michelle PL on 26/03/25.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
