22ea653fece016644b2ada8e0cba5fb05ba240b9
Michelle Pandojo Lukman <michellepandojo@gmail.com>
2025-03-27 11:55:06 +0700

    repeat component

---
+++ goppo/Views/RepeatView.swift
@@ -0,0 +1,18 @@
+//
+//  RepeatView.swift
+//  goppo
+//
+//  Created by Michelle PL on 27/03/25.
+//
+
+import SwiftUI
+
+struct RepeatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepeatView()
+}
