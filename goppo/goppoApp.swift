921637cf37a6887ced3da7f022e0c3e30571328b
rianraff <aldrian.raffi@ui.ac.id>
2025-04-30 08:23:29 +0700

    prepare to new branch

--- goppo/goppoApp.swift
+++ goppo/goppoApp.swift
@@ -14,15 +14,6 @@ struct goppoApp: App {
     
     var body: some Scene {
         WindowGroup {
-//            Tenants_Page(tenant: Tenant(id: 1,
-//                                        name: "Kedai Aneka Rasa",
-//                                        operation_time: "08.00 - 14.00",
-//                                        phone_number: "085777129912",
-//                                        va_number: "123456",
-//                                        va_bank: "qris_kedai_aneka_rasa",
-//                                        imageName : "kedai_aneka_rasa",
-//                                        qrisImageName : "BCA"
-//                                       )
             
                 SplashScreenView()
             
